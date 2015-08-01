27f9bf012086061a25836265bb0344e47f79172e
Alan Carden <acarden120@gmail.com>
2015-08-01 18:19:39 +0100

    Complete Event Controller

--- spec/features/register_app_spec.rb
+++ spec/features/register_app_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 feature 'user adds registered application' do
-  
   before do
     @user = create(:user)
     visit new_user_session_path
@@ -11,29 +10,29 @@ feature 'user adds registered application' do
     click_link 'My Registered Applications'
     click_link 'Add application'
   end
-  
+
   scenario 'successfully' do
     fill_in 'Name', with: 'Example'
     fill_in 'Url', with: 'http://example.com'
     click_button 'Save'
-    
+
     expect(page).to have_content('Example')
     expect(page).to have_content('http://example.com')
   end
-  
+
   scenario 'unsuccessfully with no name' do
     fill_in 'Name', with: ''
     fill_in 'Url', with: 'http://noname.com'
     click_button 'Save'
-    
+
     expect(page).to have_content('Error creating application. Please try again.')
   end
-  
+
   scenario 'unsuccessfully with no url' do
     fill_in 'Name', with: 'Name'
     fill_in 'Url', with: ''
     click_button 'Save'
-    
+
     expect(page).to have_content('Error creating application. Please try again.')
   end
 end
