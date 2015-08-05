21c91c28dff7a5c0d35268add6d86cbbfbb4d125
Alan Carden <acarden120@gmail.com>
2015-08-05 21:46:29 +0100

    Complete API Controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,17 +1,38 @@
 class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
+
+  before_filter :set_access_control_headers
  
   def create
     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
 
-    if registered_application == nil
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+    puts "*********************************************"
+
+    if !registered_application
       render json: "Unregistered application", status: :unprocessable_entity
     else
       @event = registered_application.events.new(event_params)
       if @event.save
+        puts "nothing to see here"
         render json: @event, status: :created
       else
-        render @event.errors, status: :unprocessable_entity
+        render "There is something strange happening here."
+#        render @event.errors, status: :unprocessable_entity
       end
     end
   end
